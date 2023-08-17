c72d2f2d538bd5025a11105e24230db56ef49cc3
girishjondhale <girishjondhale1997@gmail.com>
2023-08-16 18:13:19 -0700

    Add project files.

---
+++ Bankapp/Class2.cs
@@ -0,0 +1,26 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Bankapp
+//{
+//    internal class Class2
+//    {
+//        static void Main(string[] args)
+//        {
+//            int num;
+//            num = 19;
+//            Console.WriteLine("num :"+num);
+//            if (num % 3 == 0)
+//            {
+//                Console.WriteLine("divisible by 3");
+//            }
+//            else 
+//            {
+//                Console.WriteLine("not divisible by 3");
+//            }
+//        }
+//    }
+//}
---
+++ Bankapp/Class3.cs
@@ -0,0 +1,41 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Bankapp
+//{
+//    public class Class3
+//    {
+          
+
+//            public static void Main(string[] args)
+//            {
+//                int num = 5623;
+                
+//                while (num > 0) 
+//                {
+//                    int digit=num%10;
+//                int count = 0;
+//                for (int i = 2; i < digit; i++)
+//                {
+//                    if (digit % 10 == 0)
+//                    {
+//                        count = 1;
+//                    }
+//                }
+//                if (count == 0)
+//                {
+//                    Console.WriteLine(count);
+//                }
+//                }
+//                }
+
+//               // Console.WriteLine(countDigit(n));
+//            }
+//        }
+
+
+    
+
